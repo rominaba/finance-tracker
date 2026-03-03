87cfe68706c1799e4906fb12f392eb9e9418dffd
harunm1 <harun.mohammad2109@gmail.com>
2026-03-03 12:07:35 -0500

    Initial commit with project structure and basic docker set up

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config:
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
